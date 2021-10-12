444a27d4d4d8e14012a9b6c15a7996ae7a0d5676
unknown <tudur.buzgure@stud.ubbcluj.ro>
2021-10-12 19:07:40 +0300

    Am rezolvat problemele

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from typing import List
 def show_menu():
     print('1.Citire Date')
     print('2.Cea mai lunga subsecventa in care toate numerele au semne alternative')
-    print('3...')
+    print('3. Cea mai lunga subsecventa in care toate numerele au acelasi numar de biti de 1 in reprezentarea binara')
     print('x.Exit')
+
+
 def read_list() -> List[int]:
+    '''
+    Functia de citire a listei
+    :return:lista citita
+    '''
     list = []
     list_str = input('Dati numerele separate prin spatiu')
     list_str_split = list_str.split(' ')
@@ -16,6 +22,12 @@ def read_list() -> List[int]:
 
 
 def different_signs(a, b):
+    '''
+    Determina daca doua numere au semne diferite
+    :param a: primul numar
+    :param b: al doilea numar
+    :return: True daca numerele au semne diferite, respectiv False daca nu
+    '''
     if a <= 0 and b >=0:
         return True
     elif a >= 0 and b <= 0:
@@ -23,11 +35,13 @@ def different_signs(a, b):
     else:
         return False
 
+
 def test_different_signs():
     assert different_signs(-1, 1) == True
     assert different_signs(-1, -1) == False
     assert different_signs(1, 1) == False
     assert different_signs(1, -1) == True
+
 
 def get_longest_alternating_signs(lst: List[int]) -> List[int]:
     '''
@@ -51,6 +65,8 @@ def get_longest_alternating_signs(lst: List[int]) -> List[int]:
         return None
     result = lst[end_ind - max_len + 1: end_ind + 1]
     return result
+
+
 def test_get_longest_alternating_signs():
     assert get_longest_alternating_signs([1, -1, 1]) == [1, -1, 1]
     assert get_longest_alternating_signs([1, 1, 1]) == None
@@ -58,30 +74,55 @@ def test_get_longest_alternating_signs():
     assert get_longest_alternating_signs([1, -1, 1, 2, 3, 4, 5, 6, -1, 2, -3, 4, -5, 6]) == [6, -1, 2, -3, 4, -5, 6]
     assert get_longest_alternating_signs([-1, -1, 1]) == [-1, 1]
 
-def convert_number(n):
+
+def number_of_set_bits(num):
     '''
-    Converteste un numar dat din baza 10 in baza 2
-    Input:
-    -n: intreg
-    Output:
-    -base2Number: lista de string-uri
+    Determina numarul de biti setati ai lui num
+    :param num: numarul ai carui biti de 1 urmeaza a fi numarati
+    :return: setBits : un counter care determina numarul de biti setati dintr-un numar
     '''
-    base2Number = []
-    while n:
-        base2Number.append(str(n%2))
-        n = n // 2
-    base2Number.reverse()
-    return ''.join(base2Number)
-def test_convert_number():
-    assert convert_number(12) == "1100"
+    setBits = 0
+    while num:
+        bit = num % 2
+        num = num // 2
+        if bit == 1:
+            setBits += 1
+    return setBits
+
+def test_number_of_set_bits():
+    assert number_of_set_bits(8) == 1
+    assert number_of_set_bits(10) == 2
+    assert number_of_set_bits(12) == 2
+    assert number_of_set_bits(4) == 1
 
 
+def get_longest_same_bit_counts(lst: List[int]) -> List[int]:
+    '''
+    Determina cea mai lunga subsecventa in care toate numerele au acelasi numar de biti de 1 in reprezentarea binara
+    :param lst: lista de intregi pozitivi
+    :return: result - lista ce contine subsecventa ceruta
+    '''
+    n = len(lst)
+    max_len = 1
+    end_ind = 0
+    current_len = 1
+    for i in range(1, n):
+        if number_of_set_bits(lst[i]) == number_of_set_bits(lst[i - 1]):
+            current_len += 1
+            if current_len > max_len:
+                max_len = current_len
+                end_ind = i
+        else:
+            current_len = 1
+    if max_len == 1:
+        return lst[0]
+    result = lst[end_ind - max_len + 1: end_ind + 1]
+    return result
 
-
-
-def same_bit(a, b):
-    pass
-test_convert_number()
+def test_get_longest_same_bit_counts():
+    assert get_longest_same_bit_counts([10, 12, 14, 8]) == [10, 12]
+    assert get_longest_same_bit_counts([10, 12, 1, 2, 4, 8, 16]) == [1, 2, 4, 8, 16]
+    assert get_longest_same_bit_counts([1,10,121]) == 1
 def main():
     while True:
         show_menu()
@@ -91,14 +132,18 @@ def main():
         elif option == '2':
             print(get_longest_alternating_signs(lst))
         elif option == '3':
-            pass
+            print(get_longest_same_bit_counts(lst))
         elif option == 'x':
             break
         else:
             print('Optiune invalida')
+
+
 if __name__ == '__main__':
     main()
     test_different_signs()
     test_get_longest_alternating_signs()
+    test_number_of_set_bits()
+    test_get_longest_same_bit_counts()
 
 
